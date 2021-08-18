b36fb91dc9e3e6bd1ca457a29208dddbfa1ec75a
Omprakash Khawshi <omkhawshi0@gmail.com>
2021-08-18 16:29:29 +0530

    Features: Added update Features Features

--- src/app/emp-dashboard/emp-dashboard.component.ts
+++ src/app/emp-dashboard/emp-dashboard.component.ts
@@ -52,20 +52,37 @@ employeeData !: any;
     })
   }
 
-deleteEmployeeid(row:any){
+  deleteEmployeeid(row:any){
   this.api.deleteEmployee(row.id)
   .subscribe(res=>{
     alert("Employee Delete Successfully")
     this.getAllEmployee();
   })
-}
-onUpdate(row:any){
+ }
+  onUpdate(row:any){
+  this.employeemodelobj.id = row.id;
   this.formValue.controls['firstName'].setValue(row.firstName);
   this.formValue.controls['lastName'].setValue(row.lastName);
   this.formValue.controls['gender'].setValue(row.gender);
   this.formValue.controls['department'].setValue(row.department);
   this.formValue.controls['salary'].setValue(row.salary);
-}
-  
+ }
+ updateEmployeeDetails(){
+  this.employeemodelobj.firstName=this.formValue.value.firstName;
+  this.employeemodelobj.lastName=this.formValue.value.lastName;
+  this.employeemodelobj.gender=this.formValue.value.gender;
+  this.employeemodelobj.department=this.formValue.value.department;
+  this.employeemodelobj.salary=this.formValue.value.salary;
 
+  this.api.updateEmployee(this.employeemodelobj,this.employeemodelobj.id)
+  .subscribe(res=>{
+    alert("Employee Update Successfully")
+    let ref=document.getElementById('cancel')
+    ref?.click();
+    this.formValue.reset();
+    this.getAllEmployee();
+    });
+  
+ }
 }
+
