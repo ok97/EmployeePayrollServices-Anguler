4d434a58bfc5f266327daf0401db82bea3f173cc
Omprakash Khawshi <omkhawshi0@gmail.com>
2021-08-18 16:00:43 +0530

    Initial: Initial Commit 🎉

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { EmpDashboardComponent } from './emp-dashboard/emp-dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmpDashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/emp-dashboard/emp-dashboard.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder,FormGroup} from '@angular/forms';
+import { ApiService } from '../Service/api.service';
+import{EmployeeModel} from './employee-dashboard model';
+
+@Component({
+  selector: 'app-emp-dashboard',
+  templateUrl: './emp-dashboard.component.html',
+  styleUrls: ['./emp-dashboard.component.css']
+})
+export class EmpDashboardComponent implements OnInit {
+
+formValue !: FormGroup;
+employeemodelobj : EmployeeModel=new EmployeeModel();
+employeeData !: any;
+  constructor(private formbuilder: FormBuilder,private api:ApiService) { }
+
+  ngOnInit(): void {
+    this.formValue = this.formbuilder.group({
+      firstName :[''],
+      lastName :[''],
+      gender :[''],
+      department :[''],
+      salary :['']
+    })
+    this.getAllEmployee();
+  }
+  postEmployeeDetails(){
+    this.employeemodelobj.firstName=this.formValue.value.firstName;
+    this.employeemodelobj.lastName=this.formValue.value.lastName;
+    this.employeemodelobj.gender=this.formValue.value.gender;
+    this.employeemodelobj.department=this.formValue.value.department;
+    this.employeemodelobj.salary=this.formValue.value.salary;
+    this.api.postEmployee(this.employeemodelobj)
+    .subscribe(res=>{
+      console.log(res);
+      alert("Employee Added Successfully")
+      let ref=document.getElementById('cancel')
+      ref?.click();
+      this.formValue.reset();
+      this.getAllEmployee();
+    },
+    err=>{
+      alert("Employee Added Failed")
+    })
+  }
+
+  getAllEmployee(){
+    this.api.getEmployee()
+    .subscribe(res=>{
+      this.employeeData=res;
+    })
+  }
+
+deleteEmployeeid(row:any){
+  this.api.deleteEmployee(row.id)
+  .subscribe(res=>{
+    alert("Employee Delete Successfully")
+    this.getAllEmployee();
+  })
+}
+  
+
+}
